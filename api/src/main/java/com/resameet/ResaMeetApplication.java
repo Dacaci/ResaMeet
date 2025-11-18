83d2646c650959e095af0456500e639e3ab83f6b
Dacaci <irumizoldik21@gmail.com>
2025-11-18 18:02:24 +0100

    Classe principale de l'application

---
+++ api/src/main/java/com/resameet/ResaMeetApplication.java
@@ -0,0 +1,11 @@
+﻿package com.resameet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaMeetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResaMeetApplication.class, args);
+    }
+}
